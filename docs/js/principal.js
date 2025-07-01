5cbc619c5eb41724f9b8243781542a31a4aafdae
Arthurjose <arthurjd03@gmail.com>
2025-07-01 01:31:36 -0300

    media query
    adicionando media query para telas menores.

---
+++ docs/js/principal.js
@@ -0,0 +1,15 @@
+        /* function mudouTamanho() {
+            if (window.innerWidth >= 768) {
+                items.style.display = 'block'
+            } else {
+                items.style.display = 'none'
+            }
+        }
+            */
+        function clickmenu () {
+            if (items.style.display == 'block') {
+                items.style.display = 'none'
+            } else {
+                items.style.display = 'block'
+            }
+        }
