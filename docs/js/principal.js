8ea584f9b0852e3e469682d140a98bba6fe4643f
Arthurjose <arthurjd03@gmail.com>
2025-07-04 01:09:48 -0300

    feat(form): implementando formuário

--- docs/js/principal.js
+++ docs/js/principal.js
@@ -1,34 +1,17 @@
-        /* function mudouTamanho() {
-            if (window.innerWidth >= 768) {
-                items.style.display = 'block'
-            } else {
-                items.style.display = 'none'
-            }
-        }
-            
-        function clickmenu () {
-            if (items.style.display == 'block') {
-                items.style.display = 'none'
-            } else {
-                items.style.display = 'block'
-            }
-        }
-*/
+
 document.addEventListener("DOMContentLoaded", function () {
-  const botaoAbrir = document.getElementById("abrir-menu");  // seu botão hambúrguer
-  const botaoFechar = document.getElementById("fechar-menu"); // botão X
+  const botaoAbrir = document.getElementById("abrir-menu"); 
+  const botaoFechar = document.getElementById("fechar-menu"); 
   const menuLinks = document.querySelector(".links");
 
   function ativarMenuMobile() {
-    // apenas se for tela menor que 768px
     if (window.innerWidth < 768) {
-      botaoAbrir.style.display = 'block'; // mostra o botão
+      botaoAbrir.style.display = 'block'; 
       botaoAbrir.addEventListener("click", abrirMenu);
       botaoFechar.addEventListener("click", fecharMenu);
     } else {
-      // Em telas grandes, mostra o menu fixo e remove a classe .ativo
       menuLinks.classList.remove("ativo");
-      botaoAbrir.style.display = 'none'; // esconde o botão hamburguer
+      botaoAbrir.style.display = 'none';
     }
   }
 
@@ -41,12 +24,10 @@ document.addEventListener("DOMContentLoaded", function () {
     menuLinks.classList.remove("ativo");
     botaoAbrir.style.display = 'block';
   }
-
-  // chama a função ao carregar
   ativarMenuMobile();
-
-  // escuta redimensionamento de tela
   window.addEventListener("resize", ativarMenuMobile);
 });
+
+
 
 
